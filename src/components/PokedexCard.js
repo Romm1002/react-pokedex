f501f750d27fac0fc0ae1eb8055d4261a7a6a222
Romm1002 <romain.chaumont@hotmail.fr>
2024-03-21 11:32:44 +0100

    pokedex

--- src/components/PokedexCard.js
+++ src/components/PokedexCard.js
@@ -1,14 +1,10 @@
-// PokedexCard.js
 import React from 'react';
 
-const PokedexCard = ({ pokemon }) => {
+const PokedexCard = ({ pokemon, removeFromPokedex }) => {
     return (
         <div>
             <h3>{pokemon.name}</h3>
-            <p>Numéro: {pokemon.number}</p>
-            <p>Types: {pokemon.types.join(', ')}</p>
-            <img src={pokemon.image} alt={pokemon.name} />
-            <button>Retirer du Pokédex</button>
+            <button onClick={() => removeFromPokedex(pokemon)}>Retirer du Pokédex</button>
         </div>
     );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const PokemonCard = ({ pokemon }) => {
+    const [pokemonData, setPokemonData] = useState(null);
+
+    useEffect(() => {
+        // Récupérer les données du Pokémon à partir de son URL
+        const fetchData = async () => {
+            try {
+                // Données du Pokémon
+                const response = await fetch(pokemon.url);
+                const data = await response.json();
+
+                setPokemonData(data);
+            } catch (error) {
+                console.log('Error fetching pokemon data:', error);
+            }
+        };
+
+        fetchData();
+    }, [pokemon.url]);
+
+    // Ajouter le Pokémon au Pokédex (localStorage)
+    const addPokedex = () => {
+        // Récupération des données actuelles du Pokédex depuis le localStorage
+        const currentPokedexData = localStorage.getItem('pokedexData');
+        let pokedexData = currentPokedexData ? JSON.parse(currentPokedexData) : [];
+
+        // Ajout du Pokémon à la liste du Pokédex
+        pokedexData.push(pokemon);
+
+        // Mise à jour du localStorage avec les données du Pokédex mises à jour
+        localStorage.setItem('pokedexData', JSON.stringify(pokedexData));
+    };
+
     return (
         <div>
-            <h3>{pokemon.name}</h3>
-            <button>Ajouter au Pokédex</button>
+            {pokemonData && (
+                <div>
+                    <h3>{pokemonData.name}</h3>
+                    <button onClick={addPokedex}>Ajouter au pokédex</button>
+                </div>
+            )}
         </div>
     );
 }
