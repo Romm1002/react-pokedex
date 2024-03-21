98eddd60bd53c446e4c0238717102707d9e8b1e9
zbrwkq <81628036+zbrwkq@users.noreply.github.com>
2024-03-21 14:49:53 +0100

    pokemon details page (#6)

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,36 +1,38 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import PokemonCard from "./PokemonCard";
+import React, { useState, useEffect } from 'react';
+import PokemonCard from './PokemonCard';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import SearchBar from "./SearchBar";
 
+
 const PokemonList = () => {
-  const [pokemons, setPokemons] = useState([]);
-  const [offset, setOffset] = useState(0);
-  const [loading, setLoading] = useState(true);
+    const [pokemons, setPokemons] = useState([]);
+    const [page, setPage] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const limit = 20;
   const [searchResults, setSearchResults] = useState([]);
-  const limit = 20;
 
-  useEffect(() => {
-    setLoading(true);
-    const fetchData = async () => {
-      try {
-        const {
-          data: { results: pokemonsData },
-        } = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-        );
-        setPokemons(pokemonsData);
-
-        const pokemonDataList = [];
-        await Promise.all(
-          pokemonsData.map(async (pokemon) => {
+    useEffect(() => {
+        setLoading(true);
+        const fetchData = async () => {
             try {
-              const { data: pokemonData } = await axios.get(pokemon.url);
-              const { data: pokemonFormData } = await axios.get(
-                pokemonData.forms[0].url
-              );
-              pokemonData.forms = pokemonFormData;
-              pokemonDataList.push(pokemonData);
+                const {data: {results : pokemonsData}} = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${page  * limit}`)
+                setPokemons(pokemonsData);
+
+                const pokemonDataList = [];
+                await Promise.all(pokemonsData.map(async (pokemon) => {
+                    try {
+                        const {data: pokemonData} = await axios.get(pokemon.url);
+                        const {data: pokemonFormData} = await axios.get(pokemonData.forms[0].url);
+                        pokemonData.forms = pokemonFormData;
+                        pokemonDataList.push(pokemonData);
+                    } catch (error) {
+                        console.error('Error fetching pokemons:', error);
+                    }
+                }));
+                pokemonDataList.sort((a, b) => a.id - b.id);
+                setPokemons(pokemonDataList);
+                setLoading(false)
             } catch (error) {
               console.error("Error fetching pokemons:", error);
             }
@@ -42,17 +44,28 @@ const PokemonList = () => {
         console.error("Error fetching pokemons:", error);
       }
     };
+        fetchData();
+    }, [page]);
 
-    fetchData();
-  }, [offset]);
-
-  const handleNextPage = () => {
-    setOffset(offset + limit);
-  };
-
-  const handlePreviousPage = () => {
-    if (offset - limit >= 0) {
-      setOffset(offset - limit);
+    if (loading) {
+        return (
+            <p>Chargement...</p>
+        )
+    } else {
+        return (
+            <div className='pokemons'>
+                {pokemons.map((pokemon, index) => (
+                    <Link to={`/${pokemon.id}`}>
+                        <PokemonCard key={index} pokemon={pokemon} />
+                    </Link>
+                ))}
+                <div>
+                    <button onClick={() => setPage(page - 1)}>Page précédente</button>
+                    {page}
+                    <button onClick={() => setPage(page + 1)}>Page suivante</button>
+                </div>
+            </div>
+        );
     }
   };
 
