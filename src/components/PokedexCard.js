b56c7cddf96faf55c99aed910e5aedc10d70f93d
zbrwkq <81628036+zbrwkq@users.noreply.github.com>
2024-03-21 12:06:29 +0100

    bugfix pokedex (#4)

--- src/components/PokedexCard.js
+++ src/components/PokedexCard.js
@@ -1,30 +1,13 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const PokedexCard = ({ pokemon, removeFromPokedex }) => {
-    const [pokemonData, setPokemonData] = useState(null);
-
-    useEffect(() => {
-        // Récupérer les données du Pokémon à partir de son URL
-        const fetchData = async () => {
-            try {
-                // Données du Pokémon
-                const response = await fetch(pokemon.url);
-                const data = await response.json();
-
-                setPokemonData(data);
-            } catch (error) {
-                console.log('Error fetching pokemon data:', error);
-            }
-        };
-
-        fetchData();
-    }, [pokemon.url]);
 
     return (
         <div className='pokedex-pokemon'>
-            {pokemonData && (
+            {pokemon && (
                 <div>
-                    <h3>{pokemon.name} #{pokemonData.id}</h3>
+                    <h3>{pokemon.name} #{pokemon.id}</h3>
                     <button onClick={() => removeFromPokedex(pokemon)}>Retirer du Pokédex</button>
                 </div>
             )}
