c405a554c8145e20015fbcc1c84d86abe6529df9
zbrwkq <damien.chv78@gmail.com>
2024-03-21 15:29:14 +0100

    bugfix

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -6,70 +6,37 @@ import SearchBar from "./SearchBar";
 
 
 const PokemonList = () => {
-    const [pokemons, setPokemons] = useState([]);
-    const [page, setPage] = useState(0);
-    const [loading, setLoading] = useState(true);
-    const limit = 20;
+  const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const limit = 20;
   const [searchResults, setSearchResults] = useState([]);
 
-    useEffect(() => {
-        setLoading(true);
-        const fetchData = async () => {
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+        try {
+          const {data: {results : pokemonsData}} = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${page  * limit}`)
+
+          const pokemonDataList = [];
+          await Promise.all(pokemonsData.map(async (pokemon) => {
             try {
-                const {data: {results : pokemonsData}} = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${page  * limit}`)
-                setPokemons(pokemonsData);
-
-                const pokemonDataList = [];
-                await Promise.all(pokemonsData.map(async (pokemon) => {
-                    try {
-                        const {data: pokemonData} = await axios.get(pokemon.url);
-                        const {data: pokemonFormData} = await axios.get(pokemonData.forms[0].url);
-                        pokemonData.forms = pokemonFormData;
-                        pokemonDataList.push(pokemonData);
-                    } catch (error) {
-                        console.error('Error fetching pokemons:', error);
-                    }
-                }));
-                pokemonDataList.sort((a, b) => a.id - b.id);
-                setPokemons(pokemonDataList);
-                setLoading(false)
+              const {data: pokemonData} = await axios.get(pokemon.url);
+              pokemonDataList.push(pokemonData);
             } catch (error) {
-              console.error("Error fetching pokemons:", error);
+              console.error('Error fetching pokemons:', error);
             }
-          })
-        );
-        setPokemons(pokemonDataList);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching pokemons:", error);
+          }));
+          pokemonDataList.sort((a, b) => a.id - b.id);
+          setPokemons(pokemonDataList);
+          setLoading(false)
+        } catch (error) {
+          console.error("Error fetching pokemons:", error);
+        }
       }
-    };
-        fetchData();
-    }, [page]);
+    fetchData();
+  }, [page]);
 
-    if (loading) {
-        return (
-            <p>Chargement...</p>
-        )
-    } else {
-        return (
-            <div className='pokemons'>
-                {pokemons.map((pokemon, index) => (
-                    <Link to={`/${pokemon.id}`}>
-                        <PokemonCard key={index} pokemon={pokemon} />
-                    </Link>
-                ))}
-                <div>
-                    <button onClick={() => setPage(page - 1)}>Page précédente</button>
-                    {page}
-                    <button onClick={() => setPage(page + 1)}>Page suivante</button>
-                </div>
-            </div>
-        );
-    }
-  };
-
-  // Fonction recherche
   const handleSearch = (searchTerm) => {
     const results = pokemons.filter((pokemon) =>
       pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -78,35 +45,40 @@ const PokemonList = () => {
   };
 
   if (loading) {
-    return <p>Chargement...</p>;
+    return (
+      <p>Chargement...</p>
+    )
   } else {
     return (
-      <div>
+      <div className='pokemons'>
         <SearchBar onSearch={handleSearch} />
-        <div className="pokemons">
-          {searchResults.length > 0
-            ? searchResults.map((pokemon, index) => (
-                <PokemonCard
+        {searchResults.length > 0
+          ? searchResults.map((pokemon, index) => (
+            <Link to={`/${pokemon.id}`}>
+              <PokemonCard
                   key={index}
                   pokemon={pokemon}
                   setPokedexData={setPokemons}
                 />
-              ))
-            : pokemons.map((pokemon, index) => (
-                <PokemonCard
-                  key={index}
-                  pokemon={pokemon}
-                  setPokedexData={setPokemons}
-                />
-              ))}
-          <div>
-            <button onClick={handlePreviousPage}>Page précédente</button>
-            <button onClick={handleNextPage}>Page suivante</button>
-          </div>
+            </Link>
+          ))
+          : pokemons.map((pokemon, index) => (
+            <Link to={`/${pokemon.id}`}>
+              <PokemonCard
+                key={index}
+                pokemon={pokemon}
+                setPokedexData={setPokemons}
+              />
+            </Link>
+          ))
+        }
+        <div>
+          <button onClick={() => setPage(page - 1)}>Page précédente</button>
+          {page}
+          <button onClick={() => setPage(page + 1)}>Page suivante</button>
         </div>
       </div>
     );
   }
 };
-
 export default PokemonList;
