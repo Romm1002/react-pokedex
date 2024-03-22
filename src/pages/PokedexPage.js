e4639bfbbd6f5bcad828973ac8426fd70d7d7479
Romm1002 <78146244+Romm1002@users.noreply.github.com>
2024-03-22 12:19:21 +0100

    css2 (#13)

--- src/pages/PokedexPage.js
+++ src/pages/PokedexPage.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 
 const PokedexPage = () => {
     return (
-        <div id='pokedex'>
-            <Link to="/">Accéder a la liste</Link>
-            <h1>Mon Pokédex</h1>
+        <div className='pokemons'>
+            <div className='nav'>
+                <h1 id="title">Mon Pokédex</h1>
+                <Link to="/">
+                    <img src="https://www.pokepedia.fr/images/1/10/LogoProjetPok%C3%A9dex.png" alt="Accéder au Pokédex" width="40px" title="Accéder au Pokémons" />
+                </Link>
+            </div>
             <Pokedex />
         </div>
     );
