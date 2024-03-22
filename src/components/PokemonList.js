a6a23774bb46f11acb7677fab115737e5e89b801
zbrwkq <81628036+zbrwkq@users.noreply.github.com>
2024-03-22 12:24:33 +0100

    Damien (#14)
    * bootstrap import
    
    * fetch all pokemons
    
    * searchbar

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -6,72 +6,82 @@ import SearchBar from "./SearchBar";
 import { useSearchParams } from "react-router-dom";
 
 const PokemonList = () => {
+  const [pokemonUrls, setPokemonUrls] = useState([]);
+  const [pokemonPageUrls, setPokemonPageUrls] = useState([]);
   const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(true);
-  const limit = 20;
-  const [searchResults, setSearchResults] = useState([]);
+  const pageSize = 20;
   const [searchTerm, setSearchTerm] = useState('');
   const [searchParams] = useSearchParams();
   const page = isNaN(parseInt(searchParams.get("page"))) ? 0 : parseInt(searchParams.get("page"));
+
+  useEffect(() => {
+    const _ = ( async () =>{
+      try {
+        let next = 'https://pokeapi.co/api/v2/pokemon?limit=500&offset=0'
+        let i = 0;
+        let pokemonsData = [];
+        while (next !== null) {
+          if(i >= 5){
+            setPokemonUrls(pokemonsData)
+            throw(new Error('too many requests!'))
+          }
+          i++;
+          const {data} = await axios.get(next);
+          next = data.next;
+          pokemonsData = pokemonsData.concat(data.results)
+        }
+        setPokemonUrls(pokemonsData)
+        setPokemonPageUrls(pokemonsData)
+      } catch (error) {
+        console.error("Error fetching pokemons:", error);
+      }
+    })();
+  }, [])
+
   useEffect(() => {
     setLoading(true);
     const fetchData = async () => {
       try {
-        const {
-          data: { results: pokemonsData },
-        } = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${
-            page * limit
-          }`
+        const urls = pokemonPageUrls.slice(pageSize * page, pageSize * page + pageSize);
+        const response = await Promise.all(
+          urls.map(pokemon => axios.get(pokemon.url))
         );
-
-        const pokemonDataList = [];
-        await Promise.all(
-          pokemonsData.map(async (pokemon) => {
-            try {
-              const { data: pokemonData } = await axios.get(pokemon.url);
-              pokemonDataList.push(pokemonData);
-            } catch (error) {
-              console.error("Error fetching pokemons:", error);
-            }
-          })
-        );
-        pokemonDataList.sort((a, b) => a.id - b.id);
-        setPokemons(pokemonDataList);
+        setPokemons(response.map(data => data.data));
         setLoading(false)
       } catch (error) {
         console.error("Error fetching pokemons:", error);
       }
     };
     fetchData();
-  }, [page]);
+  }, [page, pokemonPageUrls]);
 
   useEffect(() => {
-    const results = pokemons.filter((pokemon) =>
-      pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+    const results = pokemonUrls.filter((pokemon) =>{
+      return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+    }
     );
-    setSearchResults(results);
-  }, [searchTerm, pokemons]) 
+    setPokemonPageUrls(results);
+  }, [searchTerm, pokemonUrls]) 
 
-  if (loading) {
-    return <p>Chargement...</p>;
-  } else {
-    return (
-      <div>
-        <div className="searchBar">
-          <SearchBar value ={searchTerm} onSearch={setSearchTerm} />
-        </div>
-        <div className="pokemons-list">
-          {searchTerm
-            ? searchResults.length > 0 
-                ? searchResults.map((pokemon, index) => <PokemonCard pokemon={pokemon} key={index}  />)
-                : <>Aucun pokémon ne corréspond</>
-            : pokemons.map((pokemon, index) => <PokemonCard pokemon={pokemon} key={index}  />)
+  return (
+    <div>
+      <div className="searchBar">
+        <SearchBar value ={searchTerm} onSearch={setSearchTerm} />
+      </div>
+      {loading
+      ? <p>Chargement...</p>
+      : <div className="pokemons-list">
+          {searchTerm && pokemons.length === 0
+            ? <p>Aucun pokémon ne corréspond</p> 
+            : pokemons.length === 0
+              ? <p>Un problème est survenue</p> 
+              : pokemons.map((pokemon, index) => <PokemonCard pokemon={pokemon} key={index}  />)
           }
           <Pagination page={page} />
         </div>
-      </div>
-    );
-  }
+      }
+    </div>
+  );
 };
 export default PokemonList;
